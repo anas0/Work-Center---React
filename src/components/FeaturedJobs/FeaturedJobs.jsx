d5de45cbb5b16e40d452cbfed1a1a95684a7ebdc
anas0 <ashraful0a@gmail.com>
2023-04-10 11:13:05 +0600

    Featured Jobs section design done

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { MapPinIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid'
+
+const FeaturedJobs = () => {
+    return (
+        <div>
+            <div className="text-center mt-16">
+                <h3 className='text-4xl font-bold'>Featured Jobs</h3>
+                <p className='my-10 text-gray-500 text-sm mt-3'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className='grid lg:grid-cols-2 my-16 gap-3'>
+                <div className='mx-auto lg:mx-0 bg-slate-50 shadow border border-gray-100 rounded p-5'>
+                    <img src="google.png" alt="" />
+                    <h4 className='text-base font-semibold mt-3'>Technical Database Engineer</h4>
+                    <p className='mb-3 text-gray-500 text-xs mt-2'>Google LLC</p>
+                    <div className='flex gap-2'>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Remote</p>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Full Time</p>
+                    </div>
+                    <div className='flex gap-3 py-3'>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <MapPinIcon className="h-4 w-4" />
+                            <span>Dhaka, Bangladesh</span>
+                        </p>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <CurrencyDollarIcon className="h-4 w-4" />
+                            <span>Salary : 100K - 150K</span>
+                        </p>
+                    </div>
+                    <a className="btn border-none text-xs font-semibold bg-gradient-to-r from-violet-400 to-violet-600">View Details</a>
+                </div>
+                                
+                <div className='mx-auto lg:mx-0 bg-slate-50 shadow border border-gray-100 rounded p-5'>
+                    <img src="google.png" alt="" />
+                    <h4 className='text-base font-semibold mt-3'>Technical Database Engineer</h4>
+                    <p className='mb-3 text-gray-500 text-xs mt-2'>Google LLC</p>
+                    <div className='flex gap-2'>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Remote</p>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Full Time</p>
+                    </div>
+                    <div className='flex gap-3 py-3'>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <MapPinIcon className="h-4 w-4" />
+                            <span>Dhaka, Bangladesh</span>
+                        </p>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <CurrencyDollarIcon className="h-4 w-4" />
+                            <span>Salary : 100K - 150K</span>
+                        </p>
+                    </div>
+                    <a className="btn border-none text-xs font-semibold bg-gradient-to-r from-violet-400 to-violet-600">View Details</a>
+                </div>
+                                
+                <div className='mx-auto lg:mx-0 bg-slate-50 shadow border border-gray-100 rounded p-5'>
+                    <img src="google.png" alt="" />
+                    <h4 className='text-base font-semibold mt-3'>Technical Database Engineer</h4>
+                    <p className='mb-3 text-gray-500 text-xs mt-2'>Google LLC</p>
+                    <div className='flex gap-2'>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Remote</p>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Full Time</p>
+                    </div>
+                    <div className='flex gap-3 py-3'>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <MapPinIcon className="h-4 w-4" />
+                            <span>Dhaka, Bangladesh</span>
+                        </p>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <CurrencyDollarIcon className="h-4 w-4" />
+                            <span>Salary : 100K - 150K</span>
+                        </p>
+                    </div>
+                    <a className="btn border-none text-xs font-semibold bg-gradient-to-r from-violet-400 to-violet-600">View Details</a>
+                </div>
+                                
+                <div className='mx-auto lg:mx-0 bg-slate-50 shadow border border-gray-100 rounded p-5'>
+                    <img src="google.png" alt="" />
+                    <h4 className='text-base font-semibold mt-3'>Technical Database Engineer</h4>
+                    <p className='mb-3 text-gray-500 text-xs mt-2'>Google LLC</p>
+                    <div className='flex gap-2'>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Remote</p>
+                        <p className='border border-purple-500 rounded py-1 px-2 w-fit text-xs text-purple-500'>Full Time</p>
+                    </div>
+                    <div className='flex gap-3 py-3'>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <MapPinIcon className="h-4 w-4" />
+                            <span>Dhaka, Bangladesh</span>
+                        </p>
+                        <p className='flex mb-3 text-gray-500 text-xs mt-2 gap-1'>
+                            <CurrencyDollarIcon className="h-4 w-4" />
+                            <span>Salary : 100K - 150K</span>
+                        </p>
+                    </div>
+                    <a className="btn border-none text-xs font-semibold bg-gradient-to-r from-violet-400 to-violet-600">View Details</a>
+                </div>          
+            </div>
+
+            <div className='text-center py-10'>
+                <a className="btn border-none text-xs font-semibold bg-gradient-to-r from-violet-400 to-violet-600">See All Jobs</a>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DreamJob from '../DreamJob/DreamJob';
 import JobCategory from '../JobCategory/JobCategory';
+import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
 
 const Home = () => {
     return (
         <div className='mx-20 md:mx-40'>
             <DreamJob></DreamJob>
             <JobCategory></JobCategory>
+            <FeaturedJobs></FeaturedJobs>
         </div>
     );
 };
